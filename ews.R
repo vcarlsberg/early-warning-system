5dfce2858b8100163612975eb2803c6869ddda21
vcarlsberg <v.carlsberg@gmail.com>
2019-10-22 23:57:10 +0700

    New commit using new file

--- ews.R
+++ ews.R
@@ -8,54 +8,58 @@ library(ggplot2)
 library(dplyr)
 
 #load data obligasi
-data_obligasi <- read_excel("Data_Obligasi.xlsx")
+data_obligasi <- read_excel("Data-EWS40Issuer.xlsx")
 
 #pca
-pca <- prcomp(data_obligasi[,6:27], scale = TRUE)
+pca <- prcomp(data_obligasi[,3:24], scale = TRUE)
 
-#hitung eigen value & tentukan berapa dimensi yang akan digunakan (dhi. pake 5-7 berdasarkan eigenvalue yang mendekati 1 (pake 7) or pareto 80:20 (pake 5))
+#hitung eigen value & tentukan berapa dimensi yang akan digunakan
 eig.val<-get_eigenvalue(pca)
 fviz_eig(pca)
 
-res.pca <- PCA(data_obligasi[,6:27],  graph = FALSE)
+res.pca <- PCA(data_obligasi[,3:24],  graph = FALSE)
 get_eig(res.pca)
 
 
-#data di reduksi jadi 5 variabel aja
-final.pca<-PCA(data_obligasi[,6:27],ncp=5)
+#data di reduksi jadi 6 variabel aja --> lihat dari eigen value nya mendekati 1
+final.pca<-PCA(data_obligasi[,3:24],ncp=6)
 
 #fff<-final.pca$ind$coord
 #fff$rating<-data_obligasi[,5]
 
 #hasil reduksi dari 27 data jadi 5, jadi kek begini
 fff<-as.data.frame(final.pca$ind$coord)
-fff$rating<-as.data.frame.array(data_obligasi[,5])
+fff$rating<-as.data.frame.array(data_obligasi[,25])
 
 fff<-as.data.frame(as.matrix(fff))
 
 #terus di GLM wae
-general_lin<-lm(fff$rating~
-                   fff$Dim.1+fff$Dim.2+fff$Dim.3+fff$Dim.4+fff$Dim.5)
+general_lin<-glm(rating ~ Dim.1 + Dim.2 + Dim.3+Dim.4+Dim.5+Dim.6, data = fff)
+
 summary(general_lin)
 
 predicted_category<-ceiling(general_lin[["fitted.values"]])
 
-
 df <- data.frame(data_obligasi,predicted_category)
-df<-df[order(-df$Cod_2), ]
-
-
+df<-df[order(-df$Rating), ]
+df<-data.frame(df,c(1:40))
 
 rownames(df) <- NULL
-
-#vvv<-as.data.frame(c(1:103))
-#colnames(vvv)<-c("data_ke")
-#df <- data.frame(data_obligasi,predicted_category,vvv)
-
 #ggplot(df)
 
-ggg<-filter(df,Cod_2==5)
-ggplot(data = df, mapping = aes(x = BONDS.NO., y = predicted_category, 
-                                color = as.factor(Cod_2))) + 
-  geom_point()
+# ggplot(data=df,aes(x=Cod_2,y=predicted_category))+
+#   geom_point(aes(y=predicted_category), colour="red")
+# 
+# ggplot(data = mtcars, mapping = aes(x = wt, y = mpg, color = as.factor(cyl))) + 
+#   geom_point()
+# 
+# ggplot(data = df, mapping = aes(x = Issuer, y = predicted_category, color = as.factor(predicted_category))) + 
+#   geom_point()
+# ggg<-filter(df,Cod_2==5)
 
+ggplot(data = df, mapping = aes(x = as.factor(c.1.40.), y = predicted_category, color = as.factor(Rating))) + 
+  theme(axis.text.x = element_text(angle = 90))+
+  labs(x ="Nomor Obligasi", y = "Predicted Rating",color = "Initial Rating")+
+  geom_point(aes(size = 2))
+
+#ffff<-
